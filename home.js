d4f2bf9a9596cd62922133ca396182c59d27140f
Helesh-Athukorala <hkadhmz33@gmail.com>
2025-05-14 21:53:46 +0530

    Update and rename home.html to home.js

--- home.js
+++ home.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script>
-        import { useState } from 'react';
+import { useState } from 'react';
 
 const tools = [
   { name: 'Online Notepad', url: '/tools/notepad.html' },
@@ -99,10 +92,4 @@ export default function UniKitDashboard() {
   );
 }
 
-    </script>
-</head>
-<body>
-    
-</body>
-</html>
 
